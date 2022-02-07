5720c0aa6144369d07a06b1a4b62e37bf1f141e7
junque1r4 <dev.junqueira>
2022-02-06 23:57:13 -0300

    done some tests with black(linter) for python

--- main.py
+++ main.py
@@ -5,67 +5,75 @@ from rich import print as prnt
 import json
 from urllib import parse, request, error
 import sys
-from API_KEY import API_KEY as KEY # Put your API key in the in a python file with a constant named API_KEY 
+from API_KEY import (
+    API_KEY as KEY,
+)  # Put your API key in the in a python file with a constant named API_KEY
 
-BASE_API_URL = 'http://api.openweathermap.org/data/2.5/weather'
+BASE_API_URL = "http://api.openweathermap.org/data/2.5/weather"
 console = Console()
 
 
 def read_user_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
-        description = 'Get weather and temperature information for a city!'
+        description="Get weather and temperature information for a city!"
     )
 
-    parser.add_argument(
-        "city", nargs = '+', type = str, help = 'Enter the name of the city.'
-    )
+    parser.add_argument("city", nargs="+", type=str, help="Enter the name of the city.")
 
     return parser.parse_args()
 
-def weather_query(city) -> string:
-    city_name = ' '.join(city)
+
+def weather_query(city: list) -> string:
+    city_name = " ".join(city)
     url_encoded_city_name = parse.quote_plus(city_name)
 
-    url = (
-        f'{BASE_API_URL}?q={url_encoded_city_name}&units=metric&APPID={KEY}'
-    )
+    url = f"{BASE_API_URL}?q={url_encoded_city_name}&units=metric&APPID={KEY}"
 
     return url
 
-def get_weather_data(query_url) -> dict:
-    with console.status('[bold green]Loading data...', spinner='earth'):
+
+def get_weather_data(query_url: str) -> dict:
+    with console.status("[bold green]Loading data...", spinner="earth"):
         try:
             response = request.urlopen(query_url)
         except error.HTTPError as http_error:
             if http_error.code == 404:
                 sys.exit("Can't find weather data for this place.")
             elif http_error.code == 401:
-                sys.exit('Access denied. Check your API key.')
+                sys.exit("Access denied. Check your API key.")
             else:
-                sys.exit(f'Something went wrong... error code: {http_error.code}')
+                sys.exit(f"Something went wrong... error code: {http_error.code}")
     data = response.read()
-    
+
     try:
         return json.loads(data)
     except:
-        sys.exit('Couldnt convert data into JSON.')
+        sys.exit("Couldnt convert data into JSON.")
 
-def show_weather_data(data_in_json) -> None:
-    city = data_in_json['name']
+
+def show_weather_data(data_in_json: dict) -> None:
+    city = data_in_json["name"]
     description = data_in_json["weather"][0]["description"]
     temperature = data_in_json["main"]["temp"]
-    wind_speed = data_in_json['wind']['speed']
+    wind_speed = data_in_json["wind"]["speed"]
 
-    prnt(f'[bold white]:heavy_check_mark:[bold yellow] {city}', end=' ')
-    prnt(f':cloud:[bold white] {description.capitalize()}', end=' ')
-    prnt(f':thermometer:[bold cyan] {temperature}°C', end=' ')
-    prnt(f':dash:[bold green] {wind_speed}Km/h')
+    prnt(f"[bold white]:heavy_check_mark:[bold yellow] {city}", end=" ")
+    prnt(f":cloud:[bold white] {description.capitalize()}", end=" ")
+    prnt(f":thermometer:[bold cyan] {temperature}°C", end=" ")
+    prnt(f":dash:[bold green] {wind_speed} Km/h")
 
-if __name__ == '__main__':
+
+def main() -> None:
     user_args = read_user_args()
     query_url = weather_query(user_args.city)
     weather_data = get_weather_data(query_url)
+
     show_weather_data(weather_data)
+
+
+if __name__ == "__main__":
+    main()
+
 
 """              TODOLIST            """
 # TODO: Implement some parsing for the user output, security first!
